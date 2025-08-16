b7905b110c813d673cb9ad9507912950a67db701
Michael McIntyre <mpmcintyre@yahoo.com>
2025-08-16 11:52:34 +0200

    Updated app

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>HEELLO STACEY</h1>
+      <h1>HEELLO MICHAL</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
